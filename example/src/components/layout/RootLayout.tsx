fcf7ae66df1937d8920ebb0e6b73f31a16e36449
Jack Herrington <jherr@pobox.com>
2024-04-19 10:07:39 -0700

    Add component recommendations

---
+++ example/src/components/layout/RootLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
